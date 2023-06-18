a6e7e271ac363748301e25241ee2b61c36085818
Lenovo <Lenovo@DESKTOP-CMCVBV3>
2023-06-18 10:10:15 +0300

    Добавьте файлы проекта.

---
+++ LR29_Sydorenco_202TN/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace LR29_Sydorenco_202TN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginButton = new System.Windows.Forms.Button();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.chatTextBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(560, 27);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(196, 36);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "button1";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Location = new System.Drawing.Point(560, 69);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(196, 36);
+            this.logoutButton.TabIndex = 1;
+            this.logoutButton.Text = "button2";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введіть ім\'я";
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(112, 27);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(402, 22);
+            this.userNameTextBox.TabIndex = 3;
+            // 
+            // chatTextBox
+            // 
+            this.chatTextBox.Location = new System.Drawing.Point(25, 111);
+            this.chatTextBox.Multiline = true;
+            this.chatTextBox.Name = "chatTextBox";
+            this.chatTextBox.Size = new System.Drawing.Size(731, 226);
+            this.chatTextBox.TabIndex = 4;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(560, 354);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(196, 72);
+            this.sendButton.TabIndex = 5;
+            this.sendButton.Text = "button1";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(25, 354);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(520, 72);
+            this.messageTextBox.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Змінити порт ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 36);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(232, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 36);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.chatTextBox);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.loginButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.TextBox chatTextBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ LR29_Sydorenco_202TN/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+
+namespace LR29_Sydorenco_202TN
+{
+    public partial class Form1 : Form
+    {
+        bool alive = false; // чи буде працювати потік для приймання
+        UdpClient client;
+        int LOCALPORT = 8001; // порт для приймання повідомлень        
+        int REMOTEPORT = 8001; // порт для передавання повідомлень
+        const int TTL = 20;
+        const string HOST = "235.5.5.1"; // хост для групового розсилання
+        IPAddress groupAddress; // адреса для групового розсилання
+        string userName; // ім’я користувача в чаті
+        public Form1()
+        {
+            InitializeComponent();
+            saveFileDialog1.Filter = "Text File(*.txt)|*.txt| Text Redactor(*.trt)|*.trt";
+            loginButton.Enabled = true; // кнопка входу
+            logoutButton.Enabled = false; // кнопка виходу
+            sendButton.Enabled = false; // кнопка отправки
+            chatTextBox.ReadOnly = true; // поле для повідомлень
+            groupAddress = IPAddress.Parse(HOST);
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loginButton.Text = "Вхід";
+            logoutButton.Text = "Вихід";
+            sendButton.Text = "Відправити";
+            button1.Text = "Зберегти";
+            button2.Text = "Змінити шрифт";
+            textBox1.Text = Convert.ToString(8001);
+        }
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            userName = userNameTextBox.Text;
+            userNameTextBox.ReadOnly = true;
+            if (textBox1.Text != Convert.ToString(8001))
+            {
+                LOCALPORT = Convert.ToInt32(textBox1.Text);
+                REMOTEPORT = Convert.ToInt32(textBox1.Text);
+                chatTextBox.AppendText($"[Порт змінено на {textBox1.Text}]"); 
+            }
+            try
+            {               
+                client = new UdpClient(LOCALPORT);
+                //підєднання до групового розсилання
+                client.JoinMulticastGroup(groupAddress, TTL);
+
+                // задача на приймання повідомлень
+                Task receiveTask = new Task(ReceiveMessages);
+                receiveTask.Start();
+                // перше повідомлення про вхід нового користувача
+                string message = userName + " вошел в чат";
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+                loginButton.Enabled = false;
+                logoutButton.Enabled = true;
+                sendButton.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        // метод приймання повідомлення
+        private void ReceiveMessages()
+        {
+            alive = true;
+            try
+            {
+                while (alive)
+                {
+                    IPEndPoint remoteIp = null;
+                    byte[] data = client.Receive(ref remoteIp);
+                    string message = Encoding.Unicode.GetString(data);
+                    // добавляем полученное сообщение в текстовое поле
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        string time = DateTime.Now.ToShortTimeString();
+                        chatTextBox.Text = time + " " + message + "\r\n"
+                        + chatTextBox.Text;
+                    }));
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                if (!alive)
+                    return;
+                throw;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        // обробник натискання кнопки sendButton
+
+    
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string message = String.Format("{0}: {1}", userName,
+                messageTextBox.Text);
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+                messageTextBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        // обробник натискання кнопки logoutButton
+        private void logoutButton_Click(object sender, EventArgs e)
+        {
+               ExitChat();
+        }
+        // вихід з чату
+        private void ExitChat()
+        {
+               string message = userName + " покидает чат";
+               byte[] data = Encoding.Unicode.GetBytes(message);
+               client.Send(data, data.Length, HOST, REMOTEPORT);
+               client.DropMulticastGroup(groupAddress);
+               alive = false;
+               client.Close();
+               loginButton.Enabled = true;
+               logoutButton.Enabled = false;
+               sendButton.Enabled = false;
+        }
+        // обработчик события закрытия формы
+        private void Form1_FormClosing(object sender,
+        FormClosingEventArgs e)
+        {
+            if (alive)
+            ExitChat();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            chatTextBox.Font = fontDialog1.Font;
+            chatTextBox.AppendText($"[Шрифт змінено на {chatTextBox.Font}]") ;
+            // запускаємо метод fontDialog1.ShowDialog() там встановлюємо переметри полів та присвоюємо ці значення полю TextBox1.Font
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            //Якщо при відкритті натиснемо кнопку ЗАКРИТИ то повернемось назад
+            File.WriteAllText(saveFileDialog1.FileName, chatTextBox.Text);
+            MessageBox.Show($" Файл був збережений в {saveFileDialog1.FileName} ");
+            // зчитуємо текст з TextBox.Text в saveFileDialog1.FileName і виводимо повідомлення
+        }
+    }
+}
+
